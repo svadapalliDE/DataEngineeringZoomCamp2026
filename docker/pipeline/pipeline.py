b3fc84e3cebe15d375c631fe16f730defde53ffc
svadapalliDE <gcplearning.svadapalli@gmail.com>
2026-03-26 18:22:07 +0000

    pipeline

--- docker/pipeline/pipeline.py
+++ docker/pipeline/pipeline.py
@@ -1,5 +1,12 @@
 import sys
+import pandas as pd
+
 print("arguments", sys.argv)
 
-# day = int(sys.argv[1])
-# print(f"Running pipeline for day {day}")
+month = int(sys.argv[1])
+
+df = pd.DataFrame({"day": [1,2], "num_passengers": [3,4], "month": month})
+print(df)
+df['month'] = month
+df.to_parquet(f"output_{month}.parquet")
+print(f"Running pipeline for month {month}")
